90b113a625905ae279ae707e14387b0708987710
Gashaw <gashaye12aweke@gmail.comm>
2025-01-17 05:06:23 +0300

    first commit

---
+++ src/tour.jsx
@@ -0,0 +1,4 @@
+const Tour = () => {
+  return <h2>tours</h2>;
+};
+export default Tour;
---
+++ src/tours.jsx
@@ -0,0 +1,8 @@
+const Tours = () => {
+  return (
+    <section>
+      <h2>tours</h2>
+    </section>
+  );
+};
+export default Tours;
